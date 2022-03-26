11bed53f728d4d86d72dbfcb6cafba18f627d4f5
Kevin Barnett <pdfkpb@gmail.com>
2022-03-26 14:50:44 -0700

    taking some pictures

--- bin/cubebert-eyes.cpp
+++ bin/cubebert-eyes.cpp
@@ -1,9 +1,20 @@
 #include "estimated_cube.h"
 
+#include <iostream>
+
 int main(int argc, char* argv[]) {
     EstimatedCube ec;
     ec.beginCapture();
+
+    std::cout << "Starting Snapping\r\n";
+
     ec.captureSide();
+    ec.captureSide();
+    ec.captureSide();
+    ec.captureSide();
+    ec.captureSide();
+    ec.captureSide();
+
     ec.endCapture();
 
     return 0;
--- lib/CubeRecognition/estimated_cube.cpp
+++ lib/CubeRecognition/estimated_cube.cpp
@@ -1,8 +1,10 @@
 #include "estimated_cube.h"
 
+#include <iostream>
 #include <fstream>
 
-EstimatedCube::EstimatedCube(), m_camera() {
+EstimatedCube::EstimatedCube() {
+    m_camera = new raspicam::RaspiCam();
 }
 
 EstimatedCube::~EstimatedCube() {
@@ -11,6 +13,7 @@ EstimatedCube::~EstimatedCube() {
 }
 
 void EstimatedCube::beginCapture() {
+    m_camera->setFormat(raspicam::RASPICAM_FORMAT_RGB);
     m_camera->open();
 }
 
@@ -21,7 +24,7 @@ void EstimatedCube::endCapture() {
 void EstimatedCube::captureSide() {
     m_camera->grab();
     unsigned char *data = new unsigned char[m_camera->getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB)];
-    m_camera->retrieve(data, raspicam::RASPICAM_FORMAT_RGB);
+    m_camera->retrieve(data);
     std::ofstream outFile ( "raspicam_image.ppm",std::ios::binary );
     outFile << "P6\n" << m_camera->getWidth() << " " << m_camera->getHeight() << " 255\n";
     outFile.write ( ( char* ) data, m_camera->getImageTypeSize ( raspicam::RASPICAM_FORMAT_RGB ) );
