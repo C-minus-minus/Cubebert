984cdd1f0009e40bb40f823f15d95102e46ef9cd
Kevin Barnett <pdfkpb@gmail.com>
2023-03-26 11:22:22 -0700

    implement base fipc logic

--- lib/FirmwareIPC/fipc.h
+++ lib/FirmwareIPC/fipc.h
@@ -1,12 +1,19 @@
 #ifndef FIPC_H
 #define FIPC_H
 
+#define SERIAL_PORT "/dev/ttyACM0"
+#define FILE_MODE "r+"
+
+#include <string>
+
 class FIPC {
 public:
     FIPC();
     ~FIPC();
 
+    void sendCommand(std::string cmd);
+
 private:
-}
+};
 
 #endif // FIPC_H
