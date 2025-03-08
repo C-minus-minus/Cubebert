3aff1feb302202fc308e8d898d0fb707b8b2438c
Owlhaus <kevin@owlhouse.io>
2025-03-08 09:08:23 -0800

    add a basic logger with color coding

---
+++ lib/Logger/logger.cpp
@@ -0,0 +1,47 @@
+#include "logger.h"
+
+#include <iostream>
+
+const std::string RED = "\033[31m";
+const std::string YELLOW = "\033[33m";
+// const std::string green = "\033[32m";
+const std::string CYAN = "\033[36m";
+const std::string RESET = "\033[0m";
+
+Logger::Logger(LogLevel level) : m_maxLogLevel(level) {
+}
+
+Logger Logger::info() {
+    m_logLevel = INFO;
+    return *this;
+}
+
+Logger Logger::error() {
+    m_logLevel = ERROR;
+    return *this;
+}
+
+Logger Logger::debug() {
+    m_logLevel = DEBUG;
+    return *this;
+}
+
+Logger Logger::warning() {
+    m_logLevel = WARNING;
+    return *this;
+}
+
+void Logger::message(std::string message) {
+    if (m_logLevel > m_maxLogLevel) {
+        return;
+    }
+
+    switch (m_logLevel) {
+        case ERROR: std::cout << RED <<      "[ERROR] "; break;
+        case WARNING: std::cout << YELLOW << "[WARN]  "; break;
+        case INFO: std::cout <<              "[INFO]  "; break;
+        case DEBUG: std::cout << CYAN <<     "[DEBUG] "; break;
+    }
+
+    std::cout << message << RESET << std::endl;
+}
---
+++ lib/Logger/logger.h
@@ -0,0 +1,32 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <map>
+#include <string>
+
+enum LogLevel {
+    ERROR,
+    WARNING,
+    INFO,
+    DEBUG
+};
+
+class Logger {
+public:
+    Logger(LogLevel level = LogLevel::INFO);
+
+    Logger info();
+    Logger error();
+    Logger debug();
+    Logger warning();
+
+    // Logger fields(std::map<std::string, std::stri> fields);
+
+    void message(std::string message);
+
+private:
+    LogLevel m_maxLogLevel;
+    LogLevel m_logLevel;
+};
+
+#endif
